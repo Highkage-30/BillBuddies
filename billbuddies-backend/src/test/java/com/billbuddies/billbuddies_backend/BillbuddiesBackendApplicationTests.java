93285bab95117746136a3f2a08e77e1da5b8fc72
Highkage-30 <kaushikap3001@gmail.com>
2026-01-24 14:23:04 +0530

    #0 initializer commit

---
+++ billbuddies-backend/src/test/java/com/billbuddies/billbuddies_backend/BillbuddiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billbuddies.billbuddies_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillbuddiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
